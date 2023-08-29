e55b45ff7df7ef2e0f50a9778d24e51d61b81328
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-29 11:27:00 +0530

    new changes

---
+++ src/components/DesktopNav.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import logo from "../assets/topmate.svg";
+
+const DesktopNav = () => {
+  const [subMenu, setSubMenu] = useState(false);
+  return (
+    <nav className="hidden h-full lg:flex lg:items-center lg:justify-between font-semibold px-5 xl:bg-green-500">
+      <a href="http://" target="_blank">
+        <img src={logo} alt="" className="h-10" />
+      </a>
+
+      <div className="lg:flex-1 ml-5 xl:ml-14">
+        <ul className="h-full lg:flex lg:items-center lg:gap-x-7 text-lg xl:gap-x-14 font-bold">
+          <li
+            className="relative flex gap-x-2 xl:gap-x-4 items-center"
+            onClick={() => setSubMenu((prev) => !prev)}
+          >
+            For Creators
+            <span className="block w-2 h-2 bg-black"></span>
+          </li>
+
+          {subMenu ? (
+            <>
+              <div className="absolute top-[4.5rem] w-[19rem] z-20 rounded-xl bg-white border-2 border-red-300">
+                <ul className="text-base font-semibold">
+                  <li className="h-14 m-2 rounded-md flex items-center gap-x-4 px-5 hover:bg-gray-400">
+                    <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                    Software Engineers
+                  </li>
+                  <li className="h-14 m-2 rounded-md flex items-center gap-x-4 px-5 hover:bg-gray-400">
+                    <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                    Designers
+                  </li>
+                  <li className="h-14 m-2 rounded-md flex items-center gap-x-4 px-5 hover:bg-gray-400">
+                    <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                    Mentors
+                  </li>
+                  <li className="h-14 m-2 rounded-md flex items-center gap-x-4 px-5 hover:bg-gray-400">
+                    <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                    Product Managers
+                  </li>
+                  <li className="h-14 m-2 rounded-md flex items-center gap-x-4 px-5 hover:bg-gray-400">
+                    <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                    Creators
+                  </li>
+                  <li className="h-14 m-2 rounded-md flex items-center gap-x-4 px-5 hover:bg-gray-400">
+                    <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                    Data Scientists
+                  </li>
+                  <li className="h-14 m-2 rounded-md flex items-center gap-x-4 px-5 hover:bg-gray-400">
+                    <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                    Mental Health Experts
+                  </li>
+                </ul>
+              </div>
+            </>
+          ) : null}
+          <li>
+            <a href="">Pricing</a>
+          </li>
+          <li>
+            <a href="">Podcast</a>
+          </li>
+
+          <button className="px-5 py-3.5 text-sm rounded-lg border-2 border-indigo-500">
+            Earnings calculator
+          </button>
+        </ul>
+      </div>
+
+      <div className="flex gap-x-5">
+        <button className="py-4 px-4 text-sm rounded-md border font-bold tracking-wide">
+          Login
+        </button>
+
+        <button className="py-4 px-4 text-sm rounded-md text-white bg-black font-bold tracking-wide">
+          Sign up free
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default DesktopNav;
--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,15 +1,101 @@
 import React, { useState } from "react";
+import logo from "../assets/logo.svg";
+import logoFull from "../assets/topmate.svg";
 
 const MobileNav = () => {
   const [menu, setMenu] = useState(false);
+  const [subMenu, setSubMenu] = useState(false);
+  console.log(menu);
   return (
-    <nav>
-      <button
-        className="w-14 h-14 bg-green-500 rounded-md"
-        onClick={() => setMenu((prev) => !prev)}
-      >
-        Me
-      </button>
+    <nav className="lg:hidden h-full flex items-center justify-between px-5">
+      <a href="http://" target="_blank">
+        <img src={logo} alt="" className="w-7 h-7 sm:hidden" />
+        <img src={logoFull} alt="" className="h-10 hidden sm:block" />
+      </a>
+      <div className="flex gap-x-5">
+        <button className="py-2 px-4 text-sm rounded-md text-white bg-black font-bold tracking-wide">
+          Sign up free
+        </button>
+        <button
+          className="w-10 h-10 sm:h-12 sm:w-12 bg-green-500 rounded-md"
+          onClick={() => setMenu((prev) => !prev)}
+        >
+          Me
+        </button>
+      </div>
+
+      {menu ? (
+        <>
+          <div className="absolute top-24 left-4 right-4 h-auto z-20 rounded-xl bg-white border-2 border-red-300">
+            <ul className="h-full flex flex-col justify-center items-centerfont-bold">
+              <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                Login
+              </li>
+              <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                Podcast
+              </li>
+              <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                Pricing
+              </li>
+              <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                <strong className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-red-500 to-violet-500">
+                  Earnings calculator
+                </strong>
+              </li>
+              <li
+                onClick={() => setSubMenu((prev) => !prev)}
+                className="h-14 m-2 rounded-md flex items-center justify-between text-2xl font-medium px-5 hover:bg-gray-400"
+              >
+                <div className="flex gap-x-5">
+                  <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                  For Creators
+                </div>
+                <div className="w-5 h-5 bg-black"></div>
+              </li>
+            </ul>
+            {subMenu ? (
+              <>
+                <div className="absolute w-full mt-4 z-20 rounded-xl bg-white border-2 border-red-300">
+                  <ul>
+                    <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                      <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                      Software Engineers
+                    </li>
+                    <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                      <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                      Designers
+                    </li>
+                    <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                      <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                      Mentors
+                    </li>
+                    <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                      <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                      Product Managers
+                    </li>
+                    <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                      <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                      Creators
+                    </li>
+                    <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                      <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                      Data Scientists
+                    </li>
+                    <li className="h-14 m-2 rounded-md flex items-center gap-x-5 text-2xl font-medium px-5 hover:bg-gray-400">
+                      <div className="bg-gradient-to-r from-violet-500 to-violet-900 w-9 h-9 rounded-md"></div>
+                      Mental Health Experts
+                    </li>
+                  </ul>
+                </div>
+              </>
+            ) : null}
+          </div>
+        </>
+      ) : null}
     </nav>
   );
 };
