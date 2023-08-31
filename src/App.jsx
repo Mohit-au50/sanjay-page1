a3594365e5310f2c815f46798665e92550354823
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-31 13:48:58 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Page0 from "./pages/Page0";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <main>
+      <header className="w-full h-20 bg-[#f8f7f2] fixed top-0 left-0 shadow-md">
+        <Navbar />
+      </header>
+      <Page0 />
+    </main>
+  );
 };
 
 export default App;
